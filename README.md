c245fa5245714f81405038c0964c59e06f045bc7
r611171 <edward.cheadle@cambiahealth.com>
2017-04-08 15:57:04 -0600

    S7L73 Refactor server.js

--- README.md
+++ README.md
@@ -9,4 +9,36 @@ http://mongoosejs.com/docs/guide.html
 Validator
 http://mongoosejs.com/docs/validation.html
 Schema's
-http://mongoosejs.com/docs/guide.html 
+http://mongoosejs.com/docs/guide.html
+
+PostMan
+http://getpostman.com
+
+
+//Model creation Examples
+
+// newTodo.save().then((doc) => {
+//   console.log('Saved Todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo')
+// });
+//
+// var nextTodo = new Todo({
+//   text: "Make Breakfast",
+// });
+//
+// nextTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (err) => {
+//   console.log('Unable to save Todo', err);
+// })
+// var newUser = new User({
+//   name: '    Edward',
+//   email: '  edward@echeadle.info  '
+// });
+
+// newUser.save().then((user) =>{
+//   console.log(JSON.stringify(user, undefined, 2));
+// }, (err) => {
+// console.log('Cannot add user', err);
+// });
--- server/server.js
+++ server/server.js
@@ -1,42 +1,4 @@
-var mongoose = require('mongoose')
+var {mongoose} = require('./db/mongoose');
 
-//Have mongoose use promises
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-//Model creation
-
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
-  }
-});
-
-//Example creating a Todo
-var newTodo = new Todo({
-  text: 'Cook dinner'
-});
-
-newTodo.save().then((doc) => {
-  console.log('Saved Todo', doc);
-}, (e) => {
-  console.log('Unable to save todo')
-});
-
-var nextTodo = new Todo({
-  text: "Make Breakfast",
-  completed: true,
-  completedAt: 0613162785
-});
-
-nextTodo.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-}, (err) => {
-  console.log('Unable to save Todo', err);
-})
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
