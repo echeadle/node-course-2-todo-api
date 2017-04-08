769118153abd0cf777a035466af4c728c1d5b407
r611171 <edward.cheadle@cambiahealth.com>
2017-04-08 08:02:16 -0600

    S7L65 Mongodb, count records

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -11,11 +11,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  db.collection('Todos').find({
-    _id: new ObjectID('58e88e62d9d97d462e4c76c9') 
-  }).toArray().then((docs)=> {
-    console.log('Todos');
-    console.log(JSON.stringify(docs, undefined, 2));
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('58e88e62d9d97d462e4c76c9')
+  // }).toArray().then((docs)=> {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
   }, (err) => {
     console.log('Unable to fetch todos', err);
   });
