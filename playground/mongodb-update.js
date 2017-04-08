99cad25b25259ef34cbff5556768190d5dde94cd
r611171 <edward.cheadle@cambiahealth.com>
2017-04-08 09:35:50 -0600

    S7L68 Challenge also Inc

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -7,16 +7,32 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  db.collection('Todos').findOneAndUpdate({
-    _id: new ObjectID('58e8f341f34c62ee3acc4f33')
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('58e8f341f34c62ee3acc4f33')
+  // }, {
+  //   $set:  {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('58e8f663f34c62ee3acc5195')
   }, {
     $set:  {
-      completed: true
+      name: 'Edward'
+    },
+    $inc: {
+      age: 1
     }
   }, {
     returnOriginal: false
   }).then((result) => {
     console.log(result);
   });
+
   // db.close();
 });
