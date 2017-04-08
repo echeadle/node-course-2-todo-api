3db43ed984339c3523f692dd70ecc5fd5b0c50a9
r611171 <edward.cheadle@cambiahealth.com>
2017-04-08 08:37:38 -0600

    S7L67 findOneAndDelete

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -12,10 +12,12 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log(result);
   // });
   // deleteOne
-  db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+  // fineOneAndDeleteMany
+  db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
     console.log(result);
   });
-  // fineOneAndDeleteMany
-
   // db.close();
 });
