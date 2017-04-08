a9a86f93b2687e798addecdfb48b30206cbe512f
r611171 <edward.cheadle@cambiahealth.com>
2017-04-08 07:23:46 -0600

    S7L64 E6 Object Destructuring

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,5 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 
+var user = {name: 'andrew', age: 25};
+var {name} = user;
+console.log(name);
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
@@ -15,17 +18,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   }
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
-
-  db.collection('Users').insertOne({
-    name: 'Carolyn Knight',
-    age: 39,
-    location: 'Sandy Utah'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert  user', err);
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-    console.log(result.ops[0]._id.getTimestamp());
-  });
+  //
+  // db.collection('Users').insertOne({
+  //   name: 'Carolyn Knight',
+  //   age: 39,
+  //   location: 'Sandy Utah'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert  user', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
   db.close();
 });
