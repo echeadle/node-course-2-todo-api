92a2c00cbe213c0f8376a2b04d7d7b17dd99b3f9
r611171 <edward.cheadle@cambiahealth.com>
2017-04-08 07:52:30 -0600

    S7L65 Search Mongodb with ObjectID

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -11,7 +11,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  db.collection('Todos').find({completed: false}).toArray().then((docs)=> {
+  db.collection('Todos').find({
+    _id: new ObjectID('58e88e62d9d97d462e4c76c9') 
+  }).toArray().then((docs)=> {
     console.log('Todos');
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err) => {
