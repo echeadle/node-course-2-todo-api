15f1e320c6305fcf0b51cff67e9b3c6e833ce4d4
r611171 <edward.cheadle@cambiahealth.com>
2017-04-08 07:34:56 -0600

    S7L64 E6 Object Destructuring a require statement

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,8 +1,10 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
-var user = {name: 'andrew', age: 25};
-var {name} = user;
-console.log(name);
+var obj = new ObjectID();
+
+console.log(obj);
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
