ab6e0e6611deb2a05677bb570de254bf6c12a276
r611171 <edward.cheadle@cambiahealth.com>
2017-04-09 08:26:22 -0600

    S7L77 Challenge and Add queries playgroud

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '58e9c59c3801a669cc8eede011';
+//
+// if(!ObjectID.isValid(id)) {
+//   console.log('ID is not valid');
+// }
+
+//
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo', todo);
+// }).catch((e) => console(e));
+
+User.findById('58e91dd9b9d10a54ab3d9f93').then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
