e611d4ea948bbde55ded8fa91dd5460c79013c33
r611171 <edward.cheadle@cambiahealth.com>
2017-04-09 09:00:38 -0600

    S7L78 Challange: Added route Get with Params

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -27,6 +28,21 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send(JSON.stringify(todo, undefined, 2));
+  }, (e) => {
+    res.status(400).send();
   });
 });
 
